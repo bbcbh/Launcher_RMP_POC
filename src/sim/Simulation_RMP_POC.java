917091bad42489413be8cc9a692475935eaedd4e
Bhui <Bhui@KI840G8-2.ad.nchecr.unsw.edu.au>
2025-09-29 10:52:59 +1000

    Add customised MANIFEST.MF in export JAR and USAGE message.

--- src/sim/Simulation_RMP_POC.java
+++ src/sim/Simulation_RMP_POC.java
@@ -1,8 +1,25 @@
 package sim;
 
+import java.io.IOException;
 import java.util.Properties;
 
 public class Simulation_RMP_POC extends Simulation_MetaPop {
+	
+	
+	public static void main(String[] args) throws IOException, InterruptedException {
+		final String USAGE_INFO = String.format(
+				"Usage: java %s PROP_FILE_DIRECTORY " + "<-export_skip_backup> <-printProgress> <-seedMap=SEED_MAP>\n", 
+		Simulation_RMP_POC.class.getName());		
+		
+		if (args.length < 1) {
+			System.out.println(USAGE_INFO);
+			System.exit(0);
+		}else {
+			Simulation_ClusterModelTransmission.launch(args, new Simulation_MetaPop());
+		}
+															
+				
+	}
 	
 	
 	@Override
